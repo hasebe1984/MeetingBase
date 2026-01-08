b7527eff51f6bd6f0a709948be57c86ac395053d
sasaki <intecs.2509ap.06@gmail.com>
2026-01-08 11:55:22 +0900

    1/8AMキャンセル生成サーブレット編集中

--- src/main/java/jp/co/sys/servlet/CancelCreateServlet.java
+++ src/main/java/jp/co/sys/servlet/CancelCreateServlet.java
@@ -1,6 +1,7 @@
 package jp.co.sys.servlet;
 
 import java.io.IOException;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -10,7 +11,7 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class CancelCreateServlet
  */
-@WebServlet("/CancelCreateServlet")
+@WebServlet("/CancelCreate")
 public class CancelCreateServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
@@ -18,8 +19,7 @@ public class CancelCreateServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		response.sendRedirect(request.getContextPath()+"/jsp/login.jsp");
 	}
 
 	/**
