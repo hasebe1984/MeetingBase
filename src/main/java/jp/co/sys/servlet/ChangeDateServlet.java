3c6d68abf8656d6a46cc700c2ee194666b56028a
asano <intecs.2509ap.10@gmail.com>
2026-01-08 15:19:03 +0900

    1/8 PM 追加JSP

--- src/main/java/jp/co/sys/servlet/ChangeDateServlet.java
+++ src/main/java/jp/co/sys/servlet/ChangeDateServlet.java
@@ -1,11 +1,15 @@
 package jp.co.sys.servlet;
 
 import java.io.IOException;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import jp.co.sys.util.MeetingRoom;
 
 /**
  * Servlet implementation class ChangeDateServlet
@@ -26,6 +30,28 @@ public class ChangeDateServlet extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		//リクエストパラメータの取得
+		String date = request.getParameter("date");
+		String page = request.getParameter("page");
+		
+		//sessionの取得
+		HttpSession session =request.getSession();
+		
+		//会議室管理システム取得
+		MeetingRoom meetingroom =(MeetingRoom)session.getAttribute(page);
+		
+		//利用日更新
+		//
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
 		// TODO Auto-generated method stub
 		doGet(request, response);
 	}
