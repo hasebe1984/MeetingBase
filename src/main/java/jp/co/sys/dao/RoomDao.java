fff278416cbfb6470eb09bed187e497e676bcb55
Ikeda-k04 <intecs.2509ap.04@gmail.com>
2026-01-08 15:32:52 +0900

    1/8 PM dao作成

--- src/main/java/jp/co/sys/dao/RoomDao.java
+++ src/main/java/jp/co/sys/dao/RoomDao.java
@@ -1,5 +1,32 @@
 package jp.co.sys.dao;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import jp.co.sys.bean.RoomBean;
+import jp.co.sys.util.DatabaseConnectionProvider;
+
 public class RoomDao {
+	private RoomDao() {
+	}
+
+	public static RoomBean[] findAll() {
+		RoomBean[] room = new RoomBean();
+		String sql = "SELECT * FROM reservation";
+		try (Connection db = DatabaseConnectionProvider.getConnection();
+				PreparedStatement pstmt = db.prepareStatement(sql);
+				ResultSet rs = pstmt.executeQuery()) {
+			while (rs.next()) {
+				String room1 = rs.getString("id");
+				String currentUser = rs.getString("name");
+				System.out.println("接続先DB：" + room);
+				System.out.println("ログインユーザー：" + currentUser);
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
 
 }
