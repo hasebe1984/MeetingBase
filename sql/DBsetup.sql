f6ca7cd451a732b2b2439092681114280b4bc386
Ike.k <intecs.2509ap.04@gmail.com>
2026-01-09 14:25:52 +0900

    仮変更

--- sql/DBsetup.sql
+++ sql/DBsetup.sql
@@ -1,5 +1,5 @@
 DROP USER IF EXISTS 'user'@'localhost';
--- いれば消す
+-- aaaいれば消す
 CREATE USER 'user'@'localhost' IDENTIFIED BY 'pass';
 GRANT ALL PRIVILEGES ON `meetingroomB`.* TO 'user'@'localhost';
 -- .*: ユーザー登録及び、そのデータベース内にあるすべてのテーブルへの権限付与
