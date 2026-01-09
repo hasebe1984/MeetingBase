26d98e205d99989fff2a31e33114e37a38fce12a
koyama <intecs.2509ap.15@gmail.com>
2026-01-09 10:48:14 +0900

    Bean作成

--- src/main/java/jp/co/sys/bean/ReservationBean.java
+++ src/main/java/jp/co/sys/bean/ReservationBean.java
@@ -1,5 +1,68 @@
 package jp.co.sys.bean;
 
 public class ReservationBean {
+	private String date;
+	private String end;
+	private int id;
+	private String roomId;
+	private static final long serialVersionUID = 1L;
+	private String start;
+	private String userID;
 
+	//	コンストラクタ
+	public ReservationBean() {
+	}
+
+	public ReservationBean(int id, String roomId, String date, String start, String end, String userID) {
+		super();
+		this.id = id;
+		this.roomId = roomId;
+		this.date = date;
+		this.start = start;
+		this.end = end;
+		this.userID = userID;
+	}
+
+	public ReservationBean(String roomId, String date, String start, String end, String userID) {
+		this(0, roomId, date, start, end, userID);
+		this.id = id;
+		this.roomId = roomId;
+		this.date = date;
+		this.start = start;
+		this.end = end;
+		this.userID = userID;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getRoomId() {
+		return roomId;
+	}
+
+	public String getDate() {
+		return date;
+	}
+
+	public String getStart() {
+		return start;
+	}
+
+	public String getEnd() {
+		return end;
+	}
+
+	public String getUserID() {
+		return userID;
+	}
+
+	@Override
+	public String toString() {
+		return "";
+	}
 }
--- src/main/java/jp/co/sys/dao/ReservationDao.java
+++ src/main/java/jp/co/sys/dao/ReservationDao.java
@@ -1,6 +1,30 @@
 package jp.co.sys.dao;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import jp.co.sys.bean.ReservationBean;
+
 public class ReservationDao {
-	private ReservationDao() {}
+	private ReservationDao() {
+	}
+
+//	利用日の予約を検索します
+	public static java.util.List<ReservationBean> findByDate​(java.lang.String date){
+		List<ReservationBean> rlist = new ArrayList<>();
+	    String sql = "SELECT * FROM word WHERE  word1 = ?";
+
+		
+	}
 	
+	
+//	予約を追加します
+	public static boolean insert​(ReservationBean reservation) {
+		
+	}
+	
+//	予約を削除します
+	public static boolean delete​(ReservationBean reservation) {
+		
+	}
 }
