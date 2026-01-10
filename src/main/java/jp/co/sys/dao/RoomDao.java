d67e07c34aaf2a1b3bf41b5313739b94478aa970
Ikeda1847 <ikeda1847315@gmail.com>
2026-01-10 15:47:00 +0900

    ルームダオ 動作の単体テストOK（のはず

--- src/main/java/jp/co/sys/dao/RoomDao.java
+++ src/main/java/jp/co/sys/dao/RoomDao.java
@@ -36,7 +36,7 @@ public class RoomDao {
 		String sql = "INSERT INTO room VALUES (?,?)";
 		try (Connection db = DatabaseConnectionProvider.getConnection();
 				PreparedStatement pstmt = db.prepareStatement(sql)) {
-			pstmt.setInt(1, 111111); //仮
+			pstmt.setInt(1, 2222); //仮
 			pstmt.setString(2, "仮会議室"); //仮
 			ret = pstmt.executeUpdate();
 		} catch (SQLException e) {
@@ -45,49 +45,30 @@ public class RoomDao {
 		return ret != 0;
 	}
 
-	//	public int update(String name, String id) {
-	//		String sql = "UPDATE room SET name =? WHERE id =?";
-	//		return executeSql(sql);
-	//	}
-	//
-	//	public int delete(String id) {
-	//		String sql = "DELETE FROM room WHERE id=?";
-	//		return executeSql(sql);
-	//	}
-
-	//	public int executeSql(String sql) {
-	//		Statement stmt = null;
-	//		ResultSet rs = null;
-	//		int result = 0;
-	//		try {
-	//			connect();
-	//			//②ステートメントを生成
-	//			stmt = con.createStatement();
-	//			//③SQLを実行
-	//			result = stmt.executeUpdate(sql);
-	//		} catch (Exception e) {
-	//			e.printStackTrace();
-	//		} finally {
-	//			try {
-	//				if (rs != null)
-	//					rs.close();
-	//				if (stmt != null)
-	//					stmt.close();
-	//			} catch (Exception e) {
-	//				e.printStackTrace();
-	//			}
-	//		}
-	//		disconnect();
-	//		return result;
-	//	}
-	//
-	//	public void disconnect() {
-	//		try {
-	//			//⑤DBを切断
-	//			if (con != null)
-	//				con.close();
-	//		} catch (Exception e) {
-	//			e.printStackTrace();
-	//		}
-	//	}
+	public static boolean update(String name) {
+		int ret = -1;
+		String sql = "UPDATE room SET name =? WHERE id =?";
+		try (Connection db = DatabaseConnectionProvider.getConnection();
+				PreparedStatement pstmt = db.prepareStatement(sql)) {
+			pstmt.setString(1, "変更できた会議室"); //仮
+			pstmt.setInt(2, 1111); //仮
+			ret = pstmt.executeUpdate();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return ret != 0;
+	}
+	
+	public static boolean delete(int id) {
+		int ret = -1;
+		String sql = "DELETE FROM room WHERE id=?";
+		try (Connection db = DatabaseConnectionProvider.getConnection();
+				PreparedStatement pstmt = db.prepareStatement(sql)) {
+			pstmt.setInt(1, 1111); //仮
+			ret = pstmt.executeUpdate();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return ret != 0;
+	}
 }
--- src/main/java/jp/co/sys/stub/RoomDeleteStub.java
+++ src/main/java/jp/co/sys/stub/RoomDeleteStub.java
@@ -2,9 +2,9 @@ package jp.co.sys.stub;
 
 import jp.co.sys.dao.RoomDao;
 
-public class RoomInsertStub {
+public class RoomDeleteStub {
 	public static void main(String[] args) {
-		boolean passingstatus = RoomDao.insert("テスト");
+		boolean passingstatus = RoomDao.delete(1111);
 		System.out.println(passingstatus);
 	}
 }
