08e744c7369720fa1914b99abd830bcf28242b01
Ikeda1847 <ikeda1847315@gmail.com>
2026-01-10 14:58:02 +0900

    ルームダオ　一覧表示

---
+++ src/main/java/jp/co/sys/stub/RoomInsertStub.java
@@ -0,0 +1,12 @@
+package jp.co.sys.stub;
+
+import jp.co.sys.bean.RoomBean;
+import jp.co.sys.dao.RoomDao;
+
+public class RoomInsertStub {
+	public static void main(String[] args) {
+		RoomDao.findAll("テスト");
+
+        }
+	}
+}
---
+++ src/main/java/jp/co/sys/stub/RoomListStub.java
@@ -0,0 +1,14 @@
+package jp.co.sys.stub;
+
+import jp.co.sys.bean.RoomBean;
+import jp.co.sys.dao.RoomDao;
+
+public class RoomListStub {
+	public static void main(String[] args) {
+		RoomBean list = RoomDao.findAll();
+		for (RoomBean room : list) {
+            System.out.println(room.getId());
+            System.out.println(room.getName());
+        }
+	}
+}
