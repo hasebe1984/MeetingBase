4d7940644ae1e04a40b5e26692fde940a0a6f14f
yk2 <yukikoyama609@gmail.com>
2026-01-10 19:50:42 +0900

    Update2 ReservationDao

--- src/main/java/jp/co/sys/dao/ReservationDao.java
+++ src/main/java/jp/co/sys/dao/ReservationDao.java
@@ -15,9 +15,9 @@ public class ReservationDao {
 	}
 
 	//	利用日の予約を検索します
-	public static java.util.List<ReservationBean> findByDate​(java.lang.String date) {
+	public static List<ReservationBean> findByDate​(String date) {
 		List<ReservationBean> list = new ArrayList<>();
-		String sql = "SELECT * FROM reservation WHERE  date = ?";
+		String sql = "SELECT id, roomId, date, start, end,userID FROM reservation WHERE date = ?";
 
 		try (Connection conn = DatabaseConnectionProvider.getConnection();
 				PreparedStatement pstmt = conn.prepareStatement(sql)) {
@@ -28,55 +28,73 @@ public class ReservationDao {
 			try (ResultSet rs = pstmt.executeQuery()) {
 				while (rs.next()) {
 					// 値の取得
-					int id = rs.getInt("id");
-					String roomId = rs.getString("roomId");
-					String date2 = rs.getString("date");
-					String start = rs.getString("start");
-					String end = rs.getString("end");
-					String userID = rs.getString("userID");
-					ReservationBean rb = new ReservationBean(id, roomId, date2, start, end, userID);
+					//					int id = rs.getInt("id");
+					//					String roomId = rs.getString("roomId");
+					//					String date2 = rs.getString("date");
+					//					String start = rs.getString("start");
+					//					String end = rs.getString("end");
+					//					String userID = rs.getString("userID");
+					//					ReservationBean rb = new ReservationBean(id, roomId, date2, start, end, userID);
+					ReservationBean rb = new ReservationBean(rs.getInt("id"), rs.getString("roomId"),
+							rs.getString("date"),
+							rs.getString("start"), rs.getString("end"), rs.getString("userID"));
 					list.add(rb);
 				}
 			}
-		} catch (SQLException ex) {
-			ex.printStackTrace();
+		} catch (SQLException e) {
+			e.printStackTrace();
 		}
-//	    // 空だったらnullを入れる
-//	    if (list.isEmpty()) {
-//	        return null;
-//	    }
-
+		//	    // 空だったらnullを入れる
+		//	    if (list.isEmpty()) {
+		//	        return null;
+		//	    }
 
 		return list;
 	}
 
 	//	予約を追加します
 	public static boolean insert​(ReservationBean reservation) {
-		String sql = "INSERT INTO dictionary (engword, jpword) VALUES(?, ?)";
+		String sql = "INSERT INTO reservation (id,roomId,date,start,end,userID) VALUES(?, ?, ?, ?, ?, ?)";
+		//INSERT INTO reservation (id, roomId, date, start, end, userID) VALUES (7, "0302", "2026-01-10", "09:00:00", "10:00:00", "2500003");
 		// try-with-resources構文でリソースを自動的にクローズ
-		try (Connection conn = DictionaryConnectionProvider.getConnection();
+		try (Connection conn = DatabaseConnectionProvider.getConnection();
 				PreparedStatement pstmt = conn.prepareStatement(sql)) {
 			// プレースホルダーに値を設定
-			pstmt.setString(1, dict.getEnglish());
-			pstmt.setString(2, dict.getJapanese());
+			pstmt.setInt(1, reservation.getId());
+			pstmt.setString(2, reservation.getRoomId());
+			pstmt.setString(3, reservation.getDate());
+			pstmt.setString(4, reservation.getStart());
+			pstmt.setString(5, reservation.getEnd());
+			pstmt.setString(6, reservation.getUserID());
 			//更新クエリの実行
 			int ret = pstmt.executeUpdate();
 			return ret != 0;
-		} catch (ClassNotFoundException e) {
-			e.printStackTrace();
-			System.err.println("ドライバが見つかりません。");
 		} catch (SQLException e) {
 			e.printStackTrace();
 			System.err.println("SQLに関するエラーです。");
 		}
-
 		return false;
-
 	}
 
 	//	予約を削除します
 	public static boolean delete​(ReservationBean reservation) {
+		String sql = "update reservation set isDeleted = 1 where roomId = ? and date = ? and start = ?";
+		//update reservation set isDeleted = 1 where roomId = "0302" and date = "2026-01-10" and start = "09:00:00";
+		// try-with-resources構文でリソースを自動的にクローズ
+		try (Connection conn = DatabaseConnectionProvider.getConnection();
+				PreparedStatement pstmt = conn.prepareStatement(sql)) {
+			// プレースホルダーに値を設定
+			pstmt.setString(1, reservation.getRoomId());
+			pstmt.setString(2, reservation.getDate());
+			pstmt.setString(3, reservation.getStart());
+			//更新クエリの実行
+			int ret = pstmt.executeUpdate();
+			return ret != 0;
+		} catch (SQLException e) {
+			e.printStackTrace();
+			System.err.println("SQLに関するエラーです。");
+		}
 		return false;
-
 	}
+
 }
