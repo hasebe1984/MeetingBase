6cf1ccf6d8d0c6bea92edb4e9210f8bcec57f6ac
Ikeda-k04 <intecs.2509ap.04@gmail.com>
2026-01-13 14:19:14 +0900

    こやまさんダオテスト

--- src/main/java/jp/co/sys/stub/koyama/test.java
+++ src/main/java/jp/co/sys/stub/koyama/test.java
@@ -8,9 +8,10 @@ import jp.co.sys.dao.ReservationDao;
 public class test {
 
 	public static void main(String[] args) {
-		List<ReservationBean> rb=ReservationDao.findByDate​("2026-01-01");
-		boolean in= ReservationDao.insert​(7, "0302", "2026-01-10", "09:00:00", "10:00:00", "2500003", 0);
-		boolean dl= ReservationDao.delete​(ReservationBean reservation);
+		List<ReservationBean> rb = ReservationDao.findByDate​("2026-01-01");
+		ReservationBean hoge = new ReservationBean(7, "0302", "2026-01-10", "09:00:00", "10:00:00", "2500003", 0);
+		boolean in = ReservationDao.insert​(hoge);
+		//		boolean dl= ReservationDao.delete​(ReservationBean reservation);
 
 	}
 
