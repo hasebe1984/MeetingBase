a5a50700d78bcd4ad7bf3471fc410f60435c0176
koyama <intecs.2509ap.15@gmail.com>
2026-01-13 14:12:03 +0900

    1/13pm commit

---
+++ src/main/java/jp/co/sys/stub/koyama/test.java
@@ -0,0 +1,17 @@
+package jp.co.sys.stub.koyama;
+
+import java.util.List;
+
+import jp.co.sys.bean.ReservationBean;
+import jp.co.sys.dao.ReservationDao;
+
+public class test {
+
+	public static void main(String[] args) {
+		List<ReservationBean> rb=ReservationDao.findByDate​("2026-01-01");
+		boolean in= ReservationDao.insert​(7, "0302", "2026-01-10", "09:00:00", "10:00:00", "2500003", 0);
+		boolean dl= ReservationDao.delete​(ReservationBean reservation);
+
+	}
+
+}
