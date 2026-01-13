c240d24f990c410d63781d1cb58e73e9e5c630b7
kato-1623 <intecs.2509ap.05@gmail.com>
2026-01-13 12:02:20 +0900

    1/13 AM 赤線が消えた

--- src/main/java/jp/co/sys/dao/UserDao.java
+++ src/main/java/jp/co/sys/dao/UserDao.java
@@ -14,16 +14,15 @@ public class UserDao {
 	}
 
 	//	利用者IdとPasswordの認証を行う
-	//	static UserBean	 certificate​(java.lang.String id,java.lang.String password) {
 
 	public static UserBean certificate​(java.lang.String id, java.lang.String password) {
-		UserBean user= new UserBean();
+		UserBean user = new UserBean();
 		//SQL文user_idを指定して、レコードを取得
 		String sql = "select * from user where id = ?";
 		//データベースへ接続
 		try (Connection db = DatabaseConnectionProvider.getConnection();
 				PreparedStatement pstmt = db.prepareStatement(sql)) {
-		//受け取ったIdをSQL文へ代入
+			//受け取ったIdをSQL文へ代入
 			pstmt.setString(1, id);
 			try (ResultSet rs = pstmt.executeQuery()) {
 				//			SQL文を実行して実行結果を取
@@ -31,19 +30,16 @@ public class UserDao {
 					//実行結果よりそれぞれのカラムの値を取得
 					password = rs.getString("password");
 					id = rs.getString("id");
-					user=;
+					String name = rs.getString("name");
+					String address = rs.getString("address");
+					//		String address, String id, String name, String password
+					user = new UserBean(address, id, name, password);
 				}
 			}
-			}catch (ClassNotFoundException e) {
-				return null;
-			
+
 		} catch (SQLException ex) {
 			ex.printStackTrace();
 		}
 		return user;
-		}
+	}
 }
-
-
-
-
