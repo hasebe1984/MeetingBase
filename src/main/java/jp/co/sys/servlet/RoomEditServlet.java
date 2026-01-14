bd98f540696e433c936b5a24006dd61160c5cb7d
hasebe <intecs.2509ap.07@gmail.com>
2026-01-14 15:17:13 +0900

    更新

--- src/main/java/jp/co/sys/servlet/RoomEditServlet.java
+++ src/main/java/jp/co/sys/servlet/RoomEditServlet.java
@@ -19,8 +19,6 @@ public class RoomEditServlet extends HttpServlet {
        
     public RoomEditServlet() {
         super();
-        
-        
     }
     
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
@@ -31,6 +29,10 @@ public class RoomEditServlet extends HttpServlet {
 
 		request.setCharacterEncoding("UTF-8");
 		
+		String roomNameDefault = "roomDAOでＩＤから取得";
+		String roomFloorDefault = "一覧からＩＤを取得";
+		RoomBean roomDefault = new RoomBean(roomFloorDefault, roomNameDefault);
+		
 		String action = request.getParameter("action");
 		String nextPage = "";
 		String title = request.getParameter("title");
@@ -39,35 +41,47 @@ public class RoomEditServlet extends HttpServlet {
 			title = "会議室編集";
 			nextPage = "/jsp/conferenceRoomInput.jsp";
 			
-			
 		} else if ("追加".equals(action)) {
 			title = "会議室登録";
 			nextPage = "/jsp/conferenceRoomInput.jsp";
 			
 		}
-		request.setAttribute("title", title);
-				
+		
 		String roomName = request.getParameter("roomName");
 		String roomFloor = request.getParameter("roomFloor");
-		
-		if ("決定".equals(action)) {
-	
-			nextPage = "/jsp/conferenceRoomConfirm.jsp";
-			
-		}
-		
-		if ("登録".equals(action)) {
-
-
-			roomFloor = request.getParameter("roomFloor");
-			nextPage = "/jsp/conferenceRoomed.jsp";
-			
-		}
-		
 		RoomBean room = new RoomBean(roomFloor, roomName);
+				
+		if ("決定".equals(action)) {
+			nextPage = "/jsp/conferenceRoomConfirm.jsp";	
+			
+		} else if ("戻る".equals(action)) {
+			nextPage = "/jsp/conferenceRoomInput.jsp";
+			
+		} else if ("登録".equals(action)) {
+			Boolean isSuccess = false;
+			
+			if("会議室編集".equals(title)) {
+//				isSuccess = RoomDao.update(room);
+				isSuccess = true;
+				
+			} else if ("会議室登録".equals(title)) {
+//				isSuccess = RoomDao.insert(room);
+				isSuccess = true;
+				
+			}
+			
+			if (isSuccess) {
+				nextPage = "/jsp/conferenceRoomed.jsp";
+				
+			} else {
+				nextPage = "/jsp/conferenceRoomError.jsp";	
+			
+			}
+		}
 		
 		request.setAttribute("room", room);
-		
+		request.setAttribute("roomDefault", roomDefault);
+		request.setAttribute("title", title);
 
 		request.getRequestDispatcher(nextPage).forward(request, response);
 	
