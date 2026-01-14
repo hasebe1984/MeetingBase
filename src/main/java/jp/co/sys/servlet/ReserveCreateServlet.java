0a4a6da3596e923e0a471b0e74d1871560eac8bb
asano <intecs.2509ap.10@gmail.com>
2026-01-14 15:10:15 +0900

    1/14 最終

--- src/main/java/jp/co/sys/servlet/ReserveCreateServlet.java
+++ src/main/java/jp/co/sys/servlet/ReserveCreateServlet.java
@@ -14,55 +14,76 @@ import jp.co.sys.bean.ReservationBean;
 import jp.co.sys.stub.asano.MeetingRoom;
 
 /**
- * Servlet implementation class reserveCreateServlet
+ * 会議室予約を生成するサーブレットです
  */
+
 @WebServlet("/reserveCreateServlet")
 public class ReserveCreateServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-		
-		// URLから直接アクセスした場合はログイン画面へ
-		protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-			
-			response.sendRedirect(request.getContextPath() + "/jsp/login.jsp");
-			return;
-		
-		
-	}
+
+	/**
+	 *get通信の場合はログイン画面へリダイレクト
+	 */
 	
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		response.sendRedirect(request.getContextPath() + "/jsp/login.jsp");
+		return;
+
+	}
+
+	/**
+	 *ログインの成功しているアクセス者からのリクエストを受け取り予約する会議室を
+	 *sessionに登録します
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
 		//リクエストで取得した文字をUTF-8へ
-		 	request.setCharacterEncoding("UTF-8");
-		 	
-		//リクエストパラメータの取得
-			String roomId = request.getParameter("roomId");//会議室ID
-			String start = request.getParameter("time");//利用開始時刻
-			
-		//sessionの取得
-			HttpSession session =request.getSession();
-			
-		//sessionからmeetingRoomを取得
-			MeetingRoom meetingRoom =(MeetingRoom)session.getAttribute("meetingRoom");
-			
-		//Meetingroomに予約情報の生成を依頼する
-			ReservationBean reservation = meetingRoom.createReservation(roomId,start);			
-			
-		//sessionに戻す
-			session.setAttribute("reservation", reservation);
-			//sessionに予約する会議室の情報をセット
-			session.setAttribute("room",meetingRoom.getRoom(roomId));
-		// ===== コンソール確認用 =====
-			System.out.println("MeetingRoom.date = " + meetingRoom.getDate());
-			System.out.println("roomId = " + roomId);
-			System.out.println("start = " + start);
-			System.out.println("reservation = " + reservation);
+		request.setCharacterEncoding("UTF-8");
 
 		
+		//リクエストパラメータの取得
+		String roomId = request.getParameter("roomId");//会議室ID
+		String start = request.getParameter("time");//利用開始時刻
+
+		//sessionの取得
+		HttpSession session = request.getSession();
+
+		//sessionからmeetingRoomを取得
+		MeetingRoom meetingRoom = (MeetingRoom) session.getAttribute("meetingRoom");
+
+		//Meetingroomに予約情報の生成を依頼する
+		ReservationBean reservation = meetingRoom.createReservation(roomId, start);
+		
+		try {
+			//reservation:予約情報が nullかチェック
+            if (reservation == null) {
+                throw new Exception("reservation が session に存在しません");
+            }
+						
+		
+		//sessionに戻す
+		session.setAttribute("reservation", reservation);
+		//sessionに予約する会議室の情報をセット
+		session.setAttribute("room", meetingRoom.getRoom(roomId));
+		// ===== コンソール確認用 =====
+		System.out.println("MeetingRoom.date = " + meetingRoom.getDate());
+		System.out.println("roomId = " + roomId);
+		System.out.println("start = " + start);
+		System.out.println("reservation = " + reservation);
+
 		//元の画面に戻る
-		    request.getRequestDispatcher("/jsp/reserveConfirm.jsp")
-			.forward(request, response);
-				
+		request.getRequestDispatcher("/jsp/reserveConfirm.jsp")
+				.forward(request, response);
+		} catch (Exception e) {
+			//登録失敗したら予約エラー画面へ
+			request.getRequestDispatcher("/jsp/reserveError.jsp")
+					.forward(request, response);
+		}
+
 	}
 
 }
