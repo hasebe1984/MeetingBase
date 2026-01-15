69349c759f7e9ae4f358b93d1ffd4bb315a4f244
Ikeda-k04 <intecs.2509ap.04@gmail.com>
2026-01-15 11:36:44 +0900

    MRテスト

---
+++ src/main/java/jp/co/sys/stub/ikeda/MRTest.java
@@ -0,0 +1,14 @@
+package jp.co.sys.stub.ikeda;
+
+import jp.co.sys.bean.MeetingRoom;
+
+public class MRTest {
+
+	public static void main(String[] args) {
+		MeetingRoom mr = new MeetingRoom();
+		System.out.println(mr);
+		//　ヌルポがでるけど、user.toString()に値が入れば大丈夫。
+		
+		
+	}
+}
