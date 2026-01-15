86a592b42c9a0d2b9e77886acf0aa48c7d7198b3
Ikeda1847 <ikeda1847315@gmail.com>
2026-01-15 23:07:10 +0900

    1/15 池田House　最終

--- src/main/java/jp/co/sys/stub/ikeda/MRTest.java
+++ src/main/java/jp/co/sys/stub/ikeda/MRTest.java
@@ -1,14 +1,16 @@
 package jp.co.sys.stub.ikeda;
 
 import jp.co.sys.bean.MeetingRoom;
+import jp.co.sys.bean.ReservationBean;
 
 public class MRTest {
 
 	public static void main(String[] args) {
 		MeetingRoom mr = new MeetingRoom();
-		System.out.println(mr);
-		//　ヌルポがでるけど、user.toString()に値が入れば大丈夫。
-		
-		
+		//		RoomBean aa = mr.getRoom​("0201");　OK
+		ReservationBean[][] aa = mr.getReservations();
+		System.out.println(aa);
+		//  MeetingRoom()は、ヌルポがでるけど、user.toString()に値が入れば大丈夫。
+
 	}
 }
