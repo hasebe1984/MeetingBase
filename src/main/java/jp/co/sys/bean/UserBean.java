b756945cc165e1d65fc6df024bf8be567f100267
Ikeda-k04 <intecs.2509ap.04@gmail.com>
2026-01-22 11:42:39 +0900

    JavaDoc用 Beanファイル調整

--- src/main/java/jp/co/sys/bean/UserBean.java
+++ src/main/java/jp/co/sys/bean/UserBean.java
@@ -2,17 +2,22 @@ package jp.co.sys.bean;
 
 import java.io.Serializable;
 
+/**
+ * UserBeanを定義する為のクラスです。
+ * @author 加藤博文
+ * @param pepper ハッシュ化用のペッパーデータの定数です。
+ * @param hash ペッパーデータをbyte[]変換する用の定数データです。
+ * @param iterations ハッシュ化を繰り返す回数を定義する定数です。
+ * @param keyLength 出力bitを定義する定数です。（256bit＝32バイト）
+ */
 public class UserBean implements Serializable {
 	private static final long serialVersionUID = 1L;
-	private String address;
 	private String id;
 	private String name;
 	private String password;
+	private String address;
 	private int isAdmin;
 	private int isDeleted;
-	/**
-	 * ハッシュ化用の定数準備
-	 */
 	private final String pepper = "NKkxGDiVF9i+V7m5Ww70YA==";
 	private final byte[] hash = pepper.getBytes();
 	private final int iterations = 65536;
@@ -21,26 +26,42 @@ public class UserBean implements Serializable {
 	public UserBean() {
 	}
 
+	/**
+	 * @param id userテーブルのテーブルのカラム「id」です。
+	 * @param name userテーブルのテーブルのカラム「name」です。
+	 * @param password userテーブルのテーブルのカラム「password」です。
+	 * @param address userテーブルのテーブルのカラム「address」です。
+	 * @param isAdmin userテーブルのテーブルのカラム「isAdmin」です。
+	 */
 	public UserBean(String address, String id, String name, String password, int isAdmin) {
-		super();
-		this.address = address;
 		this.id = id;
 		this.name = name;
 		this.password = password;
+		this.address = address;
 		this.isAdmin = isAdmin;
 	}
 
+	/**
+	 * @param id userテーブルのテーブルのカラム「id」です。
+	 * @param name userテーブルのテーブルのカラム「name」です。
+	 * @param password userテーブルのテーブルのカラム「password」です。
+	 * @param address userテーブルのテーブルのカラム「address」です。
+	 * @param isAdmin userテーブルのテーブルのカラム「isAdmin」です。
+	 * @param isDeleted userテーブルのテーブルのカラム「isDeleted」です。
+	 */
 	public UserBean(String address, String id, String name, String password, int isAdmin, int isDeleted) {
-		super();
-		this.address = address;
 		this.id = id;
 		this.name = name;
 		this.password = password;
+		this.address = address;
 		this.isAdmin = isAdmin;
 		this.isDeleted = isDeleted;
 	}
-
-	//		UserDao
+	
+	/**
+	 * @param id userテーブルのテーブルのカラム「id」です。
+	 * @param password userテーブルのテーブルのカラム「password」です。
+	 */
 	public UserBean(String id, String password) {
 		super();
 		this.id = id;
